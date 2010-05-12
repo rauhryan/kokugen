510440b099d5e1d04d81aeeae5ec80770d2dbf4a
Ryan Kelley <rpkelley@gmail.com>
2010-05-12 18:54:21 -0500

    Few fixes to dbChangeScripts

--- dbChangeScripts/update/0027_Fixed_CumalitiveFlowStoredProcedure.sql
+++ dbChangeScripts/update/0027_Fixed_CumalitiveFlowStoredProcedure.sql
@@ -1,13 +1,9 @@
-USE [KokugenDataDev]
-GO
 
 /****** Object:  StoredProcedure [dbo].[GetCumalitiveFlowForProject]    Script Date: 05/03/2010 12:46:22 ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetCumalitiveFlowForProject]') AND type in (N'P', N'PC'))
 DROP PROCEDURE [dbo].[GetCumalitiveFlowForProject]
 GO
 
-USE [KokugenDataDev]
-GO
 
 /****** Object:  StoredProcedure [dbo].[GetCumalitiveFlowForProject]    Script Date: 05/03/2010 12:46:22 ******/
 SET ANSI_NULLS ON
